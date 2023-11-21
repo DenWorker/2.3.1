2049d94f7f199ebadae258f46d752b5476982620
DenWorker <whiteskyD@yandex.ru>
2023-11-21 22:01:53 +0300

    Fixed from 2.3.1.

--- src/main/java/web/dao/UserDaoImp.java
+++ src/main/java/web/dao/UserDaoImp.java
@@ -1,14 +1,14 @@
 package web.dao;
 
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import web.model.User;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
-@Component
+@Repository
 public class UserDaoImp implements UserDao {
 
     @PersistenceContext
